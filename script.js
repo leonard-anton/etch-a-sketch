135c18a910077ea10ccd0aef5925f9b76bcacccf
Leonard Anton <124513400+leonard-anton@users.noreply.github.com>
2025-09-04 17:07:02 +0300

    Add "Change Grid Size" button

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const container = document.querySelector(".container");
 
-let pxNum = 16;
+const changeSizeButton = document.querySelector(".changeSizeButton")
 
-for (let i = 1; i <= pxNum * pxNum; i++) {
-    const elem = document.createElement("div");
-    elem.addEventListener("mouseover", () => elem.style.backgroundColor = "black");
-    container.appendChild(elem);
+createGrid(16);
+
+changeSizeButton.addEventListener("click", () => {
+    let count = prompt("Enter grid size (max 100)");
+    let size = count;
+    deleteGrid();
+    createGrid(size);
+})
+
+function createGrid(size) {
+    for (let i = 1; i <= size * size; i++) {
+        const elem = document.createElement("div");
+        elem.addEventListener("mouseover", () => elem.style.backgroundColor = "black");
+        container.appendChild(elem);
+    }
+}
+
+function deleteGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 }
 
