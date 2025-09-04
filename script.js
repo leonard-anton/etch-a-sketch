82138a062697162a91974654c3b0cd90c96bd672
Leonard Anton <124513400+leonard-anton@users.noreply.github.com>
2025-09-04 15:31:14 +0300

    Add the ability to draw

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ let pxNum = 16;
 
 for (let i = 1; i <= pxNum * pxNum; i++) {
     const elem = document.createElement("div");
+    elem.addEventListener("mouseover", () => elem.style.backgroundColor = "black");
     container.appendChild(elem);
 }
+
