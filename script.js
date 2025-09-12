201fce6f6cb53885cf6f179fb2ee9b067dbbe357
Leonard Anton <124513400+leonard-anton@users.noreply.github.com>
2025-09-12 20:09:17 +0300

    Change drawing
    Now you need to hold left click while drawing

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const container = document.querySelector(".container");
 
 const changeSizeButton = document.querySelector(".changeSizeButton")
 
-createGrid(16);
-
 changeSizeButton.addEventListener("click", () => {
     let count = prompt("Enter grid size (max 100)");
     let size = count;
@@ -11,17 +9,37 @@ changeSizeButton.addEventListener("click", () => {
     createGrid(size);
 })
 
+let isDrawing = false;
+
+document.addEventListener("mousedown", () => isDrawing = true);
+document.addEventListener("mouseup", () => isDrawing = false);
+
 function createGrid(size) {
     for (let i = 1; i <= size * size; i++) {
-        const elem = document.createElement("div");
-        elem.addEventListener("mouseover", () => elem.style.backgroundColor = "black");
-        container.appendChild(elem);
+        const square = document.createElement("div");
+        container.appendChild(square);
     }
+    draw();
 }
+
+function draw() {
+    const squares = container.querySelectorAll("div");
+    squares.forEach(square => {
+        square.addEventListener("mouseover", () => {
+            if (isDrawing)
+                square.style.backgroundColor = "black";
+        })
+
+        square.addEventListener("mousedown", () => square.style.backgroundColor = "black");
+    })
+}
+
 
 function deleteGrid() {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
 }
+
+createGrid(16);
 
